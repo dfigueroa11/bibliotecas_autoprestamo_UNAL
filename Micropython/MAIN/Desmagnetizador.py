7b4b6a8ec6657b9db00db741fc27794588bbd338
msalgadoh <msalgadoh@unal.edu.co>
2022-07-03 20:44:31 -0500

    comprobacion de contraseña

---
+++ Micropython/MAIN/Desmagnetizador.py
@@ -0,0 +1,55 @@
+
+import machine
+import time
+
+
+
+class desmagnetizador:
+
+    def __init__(self,fan_pin,pwm1_pin,pwm2_pin):
+        self.duty = 0.5
+        self.tperiod = 20e-3
+        self.fan = machine.Pin(fan_pin,machine.Pin.IN)
+        self.pwm1 = machine.Pin(pwm1_pin,machine.Pin.IN)
+        self.pwm2 = machine.Pin(pwm2_pin,machine.Pin.IN)
+        fan.value(0)
+        pwm1.value(0)
+        pwm2.value(0)
+
+    def turn_on_fan(self):
+        self.fan.value(1)
+
+    def turn_off_fan(self):
+        self.fan.value(0)
+
+    def drive_h_bridge(self,current_direction=0):
+        if current_direction == 1:
+            self.pwm1.value(0)
+            self.pwm2.value(1)
+            return
+        if current_direction == -1:
+            self.pwm1.value(1)
+            self.pwm2.value(0)
+            return
+        self.pwm1.value(0)
+        self.pwm2.value(0)
+        return
+
+
+    def demagnetize(self):
+        for i in range(3):
+            self.drive_h_bridge(1)
+            time.sleep(self.tperiod*self.duty)
+            self.drive_h_bridge(-1)
+            time.sleep(self.tperiod*self.duty)
+        for i in range(1:11):
+            self.drive_h_bridge(1)
+            time.sleep(self.duty*self.tperiod*(exp(-(5*(i-1)/(n-1)))))
+            self.drive_h_bridge(-1)
+            time.sleep(self.duty*self.tperiod*(exp(-(5*(i-1)/(n-1)))))
+        self.drive_h_bridge(0)
+
+    def magnetize(self):
+        self.drive_h_bridge(1)
+        time.sleep(self.tperiod*self.duty)
+        self.drive_h_bridge(0)
--- Micropython/MAIN/bibliotecas_main.py
+++ Micropython/MAIN/bibliotecas_main.py
@@ -6,7 +6,6 @@ import machine
 from ili9341 import Display, color565
 from machine import Pin, SPI
 from xglcd_font import XglcdFont
->>>>>>> 7ef04551e11667aed545aa45fbfc045f14c2332e
 from machine import SoftSPI
 import math
 #importar para pantalla
@@ -17,21 +16,12 @@ from xglcd_font import XglcdFont
 from displayLib import MyDisplay
 
 import teclado 
-#Biblioteca = 'CyT' 
-<<<<<<< HEAD
-=======
 
-#disp=MyDisplay(perifericos.get_spi())
-
-timer_1 = machine.Timer(0)
-timer_2 = machine.Timer(0)
-
->>>>>>> 7ef04551e11667aed545aa45fbfc045f14c2332e
 #COMM=Communications()
 
 #iniciar pantalla
 disp=MyDisplay(perifericos.get_spi())
-disp.printImg('bibliotecas.raw')
+disp.printImg('images\bibliotecash2.raw')
 
 #variables iniciales teclado
 palabra=''
@@ -39,64 +29,52 @@ auxiliar=''
 mensajeUsuario=''
 tecladoDisponible=True
 
-<<<<<<< HEAD
 while True:
     
     card_id_libro= perifericos.lectura(2)
     if(card_id_libro != None):
         print (card_id_libro)
         sleep(5)
-=======
-
-display.draw_image('bibliotecas1.raw', 0, 0, 240, 320)
-#while True:
-#    pass
-    
-    card_id_libro= perifericos.lectura(2)
-    print (card_id_libro)
->>>>>>> 7ef04551e11667aed545aa45fbfc045f14c2332e
         with open('IDlibro.json') as IDlibro:
-            data = json.load(IDlibro)
-            data['id'] = card_id_libro  #ESTO SE ENVIARIA AL SI
-            '''try:
-                    COMM.send(topic='bibliotecas/Validate_libro', card_id_libro=card_id_libro)
-                except OSError as e:
-                    print('OSError: Unable to sent ticket update. Please restart device.')
-            '''
+            data_libro = json.load(IDlibro)
+            data_libro['id'] = card_id_libro  #ESTO SE ENVIARIA AL SI
+            # COMM.send(topic='bibliotecas/Validate_libro', card_id_libro=card_id_libro)
+               
         with open('prueba_libro.json') as prueba_libro:
             data_prueba_libro = json.load(prueba_libro)
-        if(data_prueba_libro['IDlibro'] != "0"): ##NO OLVIDAR: COMPARACIONES CON JSON SE HACEN EN STRING
-            print (data_prueba_libro['IDlibro'])
-<<<<<<< HEAD
-            if ((data_prueba_libro['current_use'] == "True")):# En prestamo: TRUE, NO en prestamo:FALSE
-                data_prueba_libro['current_use']="False"
-=======
+
+        if(data_prueba_libro['IDlibro'] != "0"): 
 
             if ((data_prueba_libro['current_use'] == "True")):# En prestamo: TRUE, NO en prestamo:FALSE
-    #                 data_prueba_libro['current_use']="False"
->>>>>>> 7ef04551e11667aed545aa45fbfc045f14c2332e
+                #Magnetizar el libro
+                sleep(5)
+                #data_prueba_libro['current_use']="False"
+                #Enviar diccionario al SI- COMM.send(topic='bibliotecas/Validate_libro', )
+                disp.printImg('images\bibliotecash4.raw')
+          
                 print(data_prueba_libro['current_use'])
-                '''try:
-                    COMM.send(topic='bibliotecas/Validate_libro', )
-                except OSError as e:
-                    print('OSError: Unable to sent ticket update. Please restart device.')
-                '''
-<<<<<<< HEAD
-
                 
-            elif ((data_prueba_libro['current_use']=="False")):
-                
+            elif ((data_prueba_libro['current_use']=="False")):# En prestamo: TRUE, NO en prestamo:FALSE
+                disp.printImg('images\bibliotecash.raw')
                 card_id_persona= perifericos.lectura(1)
                 if(card_id_persona != None):
                     print(card_id_persona)
+
+                    with open('IDcarne.json') as IDcarne:
+                        data_carne = json.load(IDcarne)
+                        data_carne['id'] = card_id_persona  #ESTO SE ENVIARIA AL SI
                     
                     with open('prueba_persona.json') as prueba_persona:
                         data_prueba_persona = json.load(prueba_persona)
-                        data_prueba_persona['IDcarne']=card_id_persona
-                    if(data_prueba_persona['cedula'] != "0"): ##NO OLVIDAR: COMPARACIONES CON JSON SE HACEN EN STRING
-                        if(data_prueba_persona['user_type'] == "Estudiante"):
-                            print('Funciono')
+                        
+                    
+                    if(data_prueba_persona['user_type'] == "Estudiante"):
+                        print('Funciono')
+
+                        while palabra!='':
+                            tecladoDisponible=True
                             while tecladoDisponible==True:
+                                disp.printImg('images\bibliotecash1.raw')
                                 auxiliar=teclado.readLetter()
                                 if auxiliar!='enter':
                                     palabra=palabra+auxiliar
@@ -104,52 +82,37 @@ display.draw_image('bibliotecas1.raw', 0, 0, 240, 320)
                                     print(palabra)
                                     disp.printText(palabra)
                                 elif  auxiliar=='enter':
-                                    tecladoDisponible=False                            
-                            break
-                            
-=======
- 
-                 
-            elif ((data_prueba_libro['current_use']=="False")):
-                display.draw_image('bibliotecash.raw', 0, 0, 240, 320)
-                card_id_persona= perifericos.lectura(1)
-                if(card_id_persona != None):
-                    print(card_id_persona)
-                    with open('prueba_persona.json') as prueba_persona:
-                        data_prueba_persona = json.load(prueba_persona)
-                        data_prueba_persona['IDcarne']=car_id_persona
-                    if(data_prueba_persona['cedula'] != "0"): ##NO OLVIDAR: COMPARACIONES CON JSON SE HACEN EN STRING
-                        if(data_prueba_persona['user_type'] == "Estudiante"):
-                            print('Funciono')
-                            break
-                            #if((data_prueba_persona['restricciones'] != "True")
+                                    tecladoDisponible=False  
+                             pass
+                             if(palabra == data_prueba_persona['clave']):
+                                print('clave correcta')
+                                disp.printImg('images\bibliotecash6.raw')
+                                #Desmagnetizar el libro
+                                sleep(6)
+                                disp.printImg('images\bibliotecash3.raw')
+                                #data_prueba_libro['current_use']="True"
+                                #Enviar diccionario actualizado a la SI, Id libro y id user para vincular
+                             elif (palabra != data_prueba_persona['clave'])
+                                disp.printImg('images\bibliotecash5.raw')
+                                palabra=''
+                                print('Clave incorrecta')
+                        pass
+
                          
-                            #else:
-                            #    disp.printShortText('Tiene deuda o sanciones. No puede hacer prestamo de libros') #Fino
-            
->>>>>>> 7ef04551e11667aed545aa45fbfc045f14c2332e
-                        else:
-                            print('No es estudiante') #Fino
                     else:
-                        print('error')
+                        print('No es estudiante') #Fino
+                    
                 else:
                     print('No ha sido leido el carne')
             else:
                 print('El estado del libro no es claro')
         else:
             print('No se ha asignado el ID del libro al json del libro') #Fino
-<<<<<<< HEAD
-    
+   
     else:
         print('No ha sido leido el libro')
     sleep(1)   
         
 
-=======
-          
-         
- 
- 
->>>>>>> 7ef04551e11667aed545aa45fbfc045f14c2332e
  
 
